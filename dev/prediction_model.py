444ff2887366c5742ac95628ada0763b5a7504ad
RubenFranc <98653484+RubenFranc@users.noreply.github.com>
2024-11-28 05:19:36 -0500

    Update prediction_model.py

--- dev/prediction_model.py
+++ dev/prediction_model.py
@@ -3,7 +3,7 @@ from joblib import load
 class PredictionModel:
 
     def __init__(self):
-        self.model = load("clasificadorDecisionTree.joblib")
+        self.model = load("./models/clasificadorDecisionTree.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
