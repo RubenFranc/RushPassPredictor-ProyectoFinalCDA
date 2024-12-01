79bc466787851301b4c14b5d4baef17c7823f4be
RubenFranc <r.franco@uniandes.edu.co>
2024-12-01 08:12:41 -0500

    Ajuste cierre de la aplicación

--- RushPassPredictor.bat
+++ RushPassPredictor.bat
@@ -1,10 +1,14 @@
 @echo off
 
 cd /d "%~dp0react-interface"
-start cmd /K "npm run start"
+start cmd /C "npm run start && exit"
 
 cd /d "%~dp0backend"
 rem Ejecutar la aplicación Flask
-start cmd /K "python app.py"
+start cmd /C "python app.py && exit"
 
 pause
+
+rem Matar el proceso de Flask y React al cerrar el .bat
+taskkill /F /IM "node.exe" > nul 2>&1
+taskkill /F /IM "python.exe" > nul 2>&1
