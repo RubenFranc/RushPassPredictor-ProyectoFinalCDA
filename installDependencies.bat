f5c81153187e5112fbcb15a2f4aaa52142fe3eff
RubenFranc <r.franco@uniandes.edu.co>
2024-12-01 09:06:29 -0500

    Dependencias

--- installDependencies.bat
+++ installDependencies.bat
@@ -2,7 +2,7 @@
 
 rem Instalar dependencias de Python (Flask)
 cd /d "%~dp0backend"
-pip install -r requirements.txt
+start cmd /C "pip install -r requirements.txt"
 
 rem Instalar dependencias de Node.js (React)
 cd /d "%~dp0react-interface"
